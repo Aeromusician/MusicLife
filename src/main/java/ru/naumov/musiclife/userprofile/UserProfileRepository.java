cc2b1acd85fa64a254599f14b6733a240684477e
Nikita Naumov <nsnaumov@gmcs.ru>
2024-06-16 16:14:39 +0300

    profile save my events

--- src/main/java/ru/naumov/musiclife/userprofile/UserProfileRepository.java
+++ src/main/java/ru/naumov/musiclife/userprofile/UserProfileRepository.java
@@ -1,8 +1,11 @@
 package ru.naumov.musiclife.userprofile;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfileEntity, Long> {
+    @Query("select e from UserProfileEntity e where e.user.id = :userId")
+    UserProfileEntity findByUserId(Long userId);
 }
